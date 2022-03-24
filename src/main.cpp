7ccfb13aa1e56b24f76c572f84b58b900ecca7d9
tadun <56317154+tadun@users.noreply.github.com>
2022-03-24 12:42:57 +0100

    Final version

--- src/main.cpp
+++ src/main.cpp
@@ -2,18 +2,18 @@
 
 using namespace std;
 
-wxIMPLEMENT_APP(App);
+wxIMPLEMENT_APP(App); // Used instead of main
 
-    wxBEGIN_EVENT_TABLE(GameFrame, wxFrame)
-        EVT_TIMER(5, GameFrame::OnTimer)
+    wxBEGIN_EVENT_TABLE(GameFrame, wxFrame) // Creates a table that handles events based on their ID
+        EVT_TIMER(5, GameFrame::OnTimer) 
         EVT_MENU(wxID_EXIT, GameFrame::OnExit)
     wxEND_EVENT_TABLE();
 
 bool App::OnInit()
 {
-    ::wxInitAllImageHandlers();
+    ::wxInitAllImageHandlers(); // Necessary for loading bitmaps
 
-    MenuFrame *menu = new MenuFrame("Menu", wxPoint(550, 275), wxSize(150, 140));
+    MenuFrame *menu = new MenuFrame("Menu", wxPoint(550, 275), wxSize(150, 140)); // Size is adjusted once the butttons are created
     menu->Show();
 
     return true;
--- src/menuframe.cpp
+++ src/menuframe.cpp
@@ -5,6 +5,7 @@
 MenuFrame::MenuFrame(const wxString &title, const wxPoint &pos, const wxSize &size) 
     : wxFrame(NULL, wxID_ANY, title, pos, size)
 {
+    // Shows a button for each difficulty
     wxButton *button_1 = new wxButton(this, 1, "Beginner", wxPoint(0, 0), wxSize(150, 20));
     button_1->Bind(wxEVT_LEFT_DOWN, &MenuFrame::selectedOption, this);
     wxButton *button_2 = new wxButton(this, 2, "Intermediate", wxPoint(0, 30), wxSize(150, 20));
@@ -12,7 +13,7 @@ MenuFrame::MenuFrame(const wxString &title, const wxPoint &pos, const wxSize &si
     wxButton *button_3 = new wxButton(this, 3, "Expert", wxPoint(0, 60), wxSize(150, 20));
     button_3->Bind(wxEVT_LEFT_DOWN, &MenuFrame::selectedOption, this);
 
-    Fit();
+    Fit(); // Resize to window based on the size of the buttons
 }
 
 // Start the game with a given difficulty
@@ -21,8 +22,8 @@ void MenuFrame::selectedOption(wxMouseEvent& event)
     int choice = event.GetId();
 
     GameFrame *frame = new GameFrame("Minesweeper", wxPoint(550, 275), wxSize(300, 300), choice);
-    frame->Show();
+    frame->Show(); // Run the game
 
-    Close(true);
+    Close(true); // End the menu window
 }
 
